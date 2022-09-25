a852bcb78e1432e7a470df0a1e34581e49c6bfdd
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-09-25 18:38:27 +0500

    Проверка использования f-строк. Очень удобно!

--- Temp/Hello/hello.py
+++ Temp/Hello/hello.py
@@ -1,2 +1,9 @@
 msg = "УРА! Hello, World!!!"
 print(msg)
+
+
+point_one = list(map(int, input('Введите через пробел координаты X и Y точки A -> ').split()))
+point_two = list(map(int, input('Введите через пробел координаты X и Y точки B -> ').split()))
+distance_AB = round(float(((point_one[0] - point_two[0]) ** 2 + (point_one[1] - point_two[1]) ** 2) ** 0.5), 2)
+print('- A', point_one, ';', 'B', point_two, '->', distance_AB)
+print(f'- A = {point_one}; B = {point_two} -> {distance_AB}')
