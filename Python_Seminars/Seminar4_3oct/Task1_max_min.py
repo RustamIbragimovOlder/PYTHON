0a3eef844a78a3f4ee8cc7779feae4547715bd0a
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-10-04 19:59:40 +0500

    Добавлены задачи семинара №4

--- Python_Seminars/Seminar4_3oct/Task1_max_min.py
+++ Python_Seminars/Seminar4_3oct/Task1_max_min.py
@@ -27,17 +27,24 @@ def min_number(mass):
             min = i
     return min
 
-def random_list(length, min_limit, max_limit):
-    mass = [random.randint(min_limit, max_limit) for i in range(length)]
-
 import random
 length = int(input('Введите количество чисел -> '))
 min_limit = int(input('Введите нижнюю границу -> '))
 max_limit = int(input('Введите верхнюю границу -> '))
-list_cheсk = random_list
-print(f'{" ".join(map(str, list_cheсk))} -> {max_number(list_cheсk)} {min_number(list_cheсk)}')
+list_check = [random.randint(min_limit, max_limit) for i in range(length)]
+print(f'{" ".join(map(str, list_check))} -> {max_number(list_check)} {min_number(list_check)}')
 
-# Вариант коллег
+# Вариант коллег (фиговый!!! Условиям не соответствует)
 
+def input_nums(log = 'Введите строку чисел через пробел -> '):
+    try:
+        return list(map(int, input(log).split()))
+    except:
+        return input_nums('Введены не целые числа или не числа. Проверьте ввод!')
 
+def get_min_max(lst):
+    return(min(lst), max(lst))
+
+nums = input_nums()
+print(get_min_max(nums))
 
--- Python_Seminars/Seminar4_3oct/Task2_корень_кв_уравн.py
+++ Python_Seminars/Seminar4_3oct/Task2_корень_кв_уравн.py
@@ -31,6 +31,12 @@ print(f'{a}x^2 + {b}x + c = 0')
 roots_quadratic_equation(a, b, c)
 
 
-# Вариант 2 Встроенная функция
+# Вариант 2 Встроенная функция ???
 
 import sympy
+x = sympy.Symbol('x')
+f = input()
+print(sympy.solve(f, x))
+# x ** 2 + 4 * x + 4 здесь цифры - это коэффициенты (a = 1, b = 4, c = 4)
+# вводить надо x ** 2 + 4 * x + 4
+
