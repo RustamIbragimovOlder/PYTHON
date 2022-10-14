fb54080b04e4bd4c1a52a9081fab94177fde423b
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-10-14 08:50:50 +0500

    Решения задач ПЗ №6 дополнены

--- Python_PracticalTasks/PT6/Task3.py
+++ Python_PracticalTasks/PT6/Task3.py
@@ -1,6 +1,8 @@
 # Напишите программу, которая принимает на вход два числа
 # и проверяет, является ли одно число квадратом другого.
 
+
+# 1. Мой вариант
 data = list(map(int, input('Введите два числа через пробел => ').split()))
 
 def square_check(a, b):
@@ -9,4 +11,11 @@ def square_check(a, b):
     else: print(False)
 
 square_check(data[0], data[1])
+
+# 1. Вариант преподавателя
+
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+x = lambda a, b: a**2 == b or b**2 == a
+print(x(a, b))
 
