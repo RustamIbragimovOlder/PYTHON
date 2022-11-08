4f8a4ce446c48821bcb9bf8d5fe2cc81d0515d4f
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-11-08 11:01:24 +0500

    Промежуточные изменения

--- Python_Seminars/Seminar9_20oct/main.py
+++ Python_Seminars/Seminar9_20oct/main.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bots_commands import *
 
 
-app = ApplicationBuilder().token("5715548573:AAFHStsDcP_U9tfw_99Yguoic-4BN-Eo4VM").build()
+app = ApplicationBuilder().token("TOKEN").build()
 
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("text_ed", text_editing))
